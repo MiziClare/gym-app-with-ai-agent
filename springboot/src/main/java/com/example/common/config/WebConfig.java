7f8bde1c2622363d9ecb02b40f14fd2b6f42d95b
Xiaoyi <mizi99207@gmail.com>
2025-02-18 08:43:48 +0000

    Scaffolding is complete.

---
+++ springboot/src/main/java/com/example/common/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class WebConfig implements  WebMvcConfigurer {
+
+    @Resource
+    private JwtInterceptor jwtInterceptor;
+
+    // 加自定义拦截器JwtInterceptor，设置拦截规则
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/",
+                        "/login",
+                        "/register",
+                        "/files/**",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v2/api-docs",
+                        "/webjars/**"
+                );
+    }
+}
