a83dff255bcaf7ca55f968c666d4be15cbd8f1a7
Xiaoyi <mizi99207@gmail.com>
2025-05-05 07:13:19 +0100

    Comment.

--- springboot/src/main/java/com/example/common/config/WebConfigurer.java
+++ springboot/src/main/java/com/example/common/config/WebConfigurer.java
@@ -8,14 +8,14 @@ public class WebConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 配置静态资源处理，如果已经配置过可以忽略
+        // Configure static resource handling, if already configured, ignore
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // 将所有未匹配的路径都映射到 index.html
+        // Map all unmatched paths to index.html
         registry.addViewController("/{spring:[a-zA-Z0-9-]+}")
                 .setViewName("forward:/index.html");
         registry.addViewController("/**/{spring:[a-zA-Z0-9-]+}")
